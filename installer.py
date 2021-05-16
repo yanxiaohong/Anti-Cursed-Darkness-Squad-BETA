95b394842ad727cd64ff0d7a9411bba0d95a94a1
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-05-16 13:52:54 +0200

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,32 @@
+# Installer
+# Run by sudo installer.py
+# chmod +x installer.py
+# python installer.py
+
+import os
+import sys
+
+os.system('sudo su')
+os.system('sudo apt update')
+os.system('sudo apt install nodejs')
+os.system('sudo apt install npm')
+os.system('npm install')
+os.system('sudo npm install')
+os.system('sudo apt install python-minimal')
+os.system('sudo apt install python3')
+os.system('sudo apt install python-pip')
+os.system('sudo apt install python3-pip')
+os.system('sudo npm install fs')
+os.system('sudo npm install cluster')
+os.system('sudo npm install random-useragent')
+os.system('sudo npm install crypto-random-string')
+os.system('sudo npm install path')
+os.system('sudo npm install cloudflare-bypasser')
+os.system('sudo npm install net')
+os.system('sudo npm install request')
+os.system('sudo npm install zombie')
+os.system('sudo npm install cloudscraper')
+os.system('pip3 install requests')
+os.system('pip3 install colorama')
+
+print("Installed")
