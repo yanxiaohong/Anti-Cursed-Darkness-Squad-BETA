fb23a303fc87738a01febb598e14373eb1f4b0a6
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-05-27 18:53:12 +0200

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+print("Open this installer only in where your Anti-Cursed-Darkness-Squad-BETA is installed!")
+
+sure = input("You sure that you are in the correct folder? Press enter to start installation: )
+
+os.system('sudo apt update')
+os.system('apt install git')
+os.system('sudo apt install nodejs')
+os.system('sudo apt install npm')
+os.system('sudo npm install cloudflare-bypasser')
+os.system(sudo npm install crypto-random-string'')
+os.system('sudo npm install random-string')
+os.system('sudo npm install random-useragent')
+os.system('sudo npm install fs')
+os.system('sudo npm install url')
+os.system('sudo npm install zombie')
+os.system('sudo npm install eval')
+os.system('sudo npm install cloudscraper')
+os.system('npm install')
+os.system('sudo apt install python3')
+os.system('pip3 install -r requirements.txt')
+os.system('sudo apt install python-minimal')
+os.system('sudo apt install perl')
+
+
+print("\n\n")
+print("If this didn't work, then install it manual. See readme.md for instructions")
