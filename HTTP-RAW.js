11e03518d38df8992059183d9133ba2d77664e5a
z3ntl3 root <48758770+Z3NTL3@users.noreply.github.com>
2021-06-30 18:29:49 +0200

    Update HTTP-RAW.js

--- HTTP-RAW.js
+++ HTTP-RAW.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const url = require('url');
 const net = require('net');
 if (process.argv.length <= 2) {
-	console.log("node HTTP-RAW.js url time");
+	
 	console.log("Edit by emp001");
 	process.exit(-1);
 }
